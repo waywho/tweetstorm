fda78c450ee4d23d763f930159a3b5f055f0540a
waywho <walzerfan@yahoo.com>
2017-10-04 22:31:28 +0100

    init and a bit more

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (req, res) {
+	var acceptsHTML = req.accepts('html');
+	if(acceptsHTML) {
+		res.sendFile(__dirname + '/dist/index.html');
+	}
+});
+
+// Start the app by listening on the default
+// Heroku port
+
+app.listen(process.env.PORT || 8080);
