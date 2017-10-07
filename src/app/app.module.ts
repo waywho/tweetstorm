469c23912d91810aab74559a5b063080dbdaa603
waywho <walzerfan@yahoo.com>
2017-10-07 21:31:29 +0100

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+import { RouterModule, Routes } from '@angular/router'; 
+
+const appRoutes: Routes = [
+	{ path: '', component: MainPageComponent },
+	{ path: 'about', component: AboutPageComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
